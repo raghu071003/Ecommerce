53184e2bf7ac22ff088c96ba6ed292a8dc0ce864
Sai Raghu Vamshi Koneti <131893227+raghu071003@users.noreply.github.com>
2024-10-25 18:03:23 +0530

    Fixed

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -9,7 +9,6 @@ app.use(cors({
     credentials:true
 }))
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
